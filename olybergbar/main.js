93205a72a34668e2fdd8e77eb794c20a28caa55f
Maximilian <miller.deutschland@gmail.com>
2022-05-17 14:31:33 +0200

    added olybergbar page

---
+++ olybergbar/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $( "#signup" ).submit(function( event ) {
+    event.preventDefault();
+    $.get("https://api.countapi.xyz/hit/leistungszentrum/olybar010622", function(data, status){
+      $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
+      $(" #signups" ).html(data['value'] + " Teilnehmer");
+      alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
+    });
+  });
+  $.get("https://api.countapi.xyz/get/leistungszentrum/olybar010622", function(data, status){
+    $(" #signups" ).html(data['value'] + " Teilnehmer");
+  });
+});
