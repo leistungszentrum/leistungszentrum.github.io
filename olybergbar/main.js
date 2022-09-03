6e807e85178b430333e0d4431f5e625317b8b84a
leistungszentrum <74734360+leistungszentrum@users.noreply.github.com>
2022-09-03 16:26:58 +0200

    changed counter to Indian summer opening

--- olybergbar/main.js
+++ olybergbar/main.js
@@ -1,13 +1,13 @@
 $(function() {
   $( "#signup" ).submit(function( event ) {
     event.preventDefault();
-    $.get("https://api.countapi.xyz/hit/leistungszentrum/olybar010622", function(data, status){
+    $.get("https://api.countapi.xyz/hit/leistungszentrum/olybar140922", function(data, status){
       $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
       $(" #signups" ).html(data['value'] + " Teilnehmer");
       alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
     });
   });
-  $.get("https://api.countapi.xyz/get/leistungszentrum/olybar010622", function(data, status){
+  $.get("https://api.countapi.xyz/get/leistungszentrum/olybar140922", function(data, status){
     $(" #signups" ).html(data['value'] + " Teilnehmer");
   });
 });
