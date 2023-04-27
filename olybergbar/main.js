bad774df6fd851c5b0171d393092b217d86ab90e
Maximilian Miller <miller.deutschland@gmail.com>
2023-04-27 09:52:08 +0200

    Update main.js
    Changed counter api

--- olybergbar/main.js
+++ olybergbar/main.js
@@ -1,13 +1,21 @@
 $(function() {
+  var board_id = "lsbtdyyhxcpzp";
+  var player_id = 7194647;
   $( "#signup" ).submit(function( event ) {
     event.preventDefault();
-    $.get("https://api.countapi.xyz/hit/leistungszentrum/olybar240523", function(data, status){
+//     $.get("https://api.countapi.xyz/hit/leistungszentrum/olybar240523", function(data, status){
+//       $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
+//       $(" #signups" ).html(data['value'] + " Teilnehmer");
+//       alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
+//     });
+    $.post(`https://keepthescore.co/api/${board_id}/score/`, { "player_id": player_id, "score": 1})
+    .done(function(data, status){
       $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
-      $(" #signups" ).html(data['value'] + " Teilnehmer");
+      $(" #signups" ).html(data['players'][0]["score"] + " Teilnehmer");
       alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
     });
   });
-  $.get("https://api.countapi.xyz/get/leistungszentrum/olybar240523", function(data, status){
-    $(" #signups" ).html(data['value'] + " Teilnehmer");
+  $.get(`https://keepthescore.co/api/${board_id}/board/`, function(data, status){
+    $(" #signups" ).html(data['players'][0]["score"] + " Teilnehmer");
   });
 });
