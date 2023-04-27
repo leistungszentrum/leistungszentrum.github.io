c1f910cfa55ab1fa3f1c8b4b4e7e746fcde43596
Maximilian Miller <miller.deutschland@gmail.com>
2023-04-27 16:16:33 +0200

    Update main.js
    Fixed bug in counter API response processing

--- olybergbar/main.js
+++ olybergbar/main.js
@@ -9,12 +9,12 @@ $(function() {
     async: false,
     data: JSON.stringify({ "player_id": player_id, "score": 1}),
     contentType: "application/json",
-        complete: function (data) {
-            $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
-            $(" #signups" ).html(data['players'][0]["score"] + " Teilnehmer");
-            alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
-            wait = false;
-        }
+    })
+    .done(function(data, status){
+      $( ".button" ).prop("disabled", true).addClass('button-disabled').removeClass('w-button');
+      $(" #signups" ).html(data['value'] + " Teilnehmer");
+      $(" #signups" ).html(data['players'][0]["score"] + " Teilnehmer");
+      alert( "Du bist dabei / You are in / Sinä olet siellä / 你在那里" );
     });
   });
   $.get(`https://keepthescore.co/api/${board_id}/board/`, function(data, status){
