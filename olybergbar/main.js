e20de7604a44a2ebd0031e58b9e3ba5e73f6e710
Maximilian Miller <miller.deutschland@gmail.com>
2023-05-26 13:05:57 +0200

    Update main.js

--- olybergbar/main.js
+++ olybergbar/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  var board_id = "lsbtdyyhxcpzp";
+  var board_id = "qmzcvhcljpmbe";
   var player_id = 7194647;
   $( "#signup" ).submit(function( event ) {
     event.preventDefault();
